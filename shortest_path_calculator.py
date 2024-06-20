7d00782419d0b59bd90988da01e7fa38c73519ba
Igor Malyi <igor.malyi@pm.me>
2024-06-20 10:55:58 +0200

    fix

--- shortest_path_calculator.py
+++ shortest_path_calculator.py
@@ -24,4 +24,4 @@ class PathCalculator:
             for residential_building, poi, dist in zip(residential_buildings, pois, dists):
                 self.results.add(residential_building, poi, Distance(dist))
             i += 1
-            self.results.save_to_db()
+        self.results.save_to_db()
